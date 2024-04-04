5a657cc79eed07aaead7a5e9c76b5a01a5f25d67
sejaltapadiya <sejal.178@iiitb.ac.in>
2024-04-04 19:35:58 +0530

    Updated sidebar

--- FRONTEND/src/Components/SidebarData.jsx
+++ FRONTEND/src/Components/SidebarData.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import { FaUserDoctor } from "react-icons/fa6";
-import { TbReportSearch } from "react-icons/tb";
-import { FaRegUser } from "react-icons/fa";
+import PersonalVideoIcon from '@mui/icons-material/PersonalVideo';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 import DraftsIcon from '@mui/icons-material/Drafts';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
-import { BsPeopleFill } from "react-icons/bs";
-import { PiNotebookBold } from "react-icons/pi";
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PeopleIcon from '@mui/icons-material/People';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import './Navbar.css';
+
 
 export const SidebarData = {
   admin: [
@@ -20,105 +24,168 @@ export const SidebarData = {
     {
       title: 'Doctor',
       path: '/admin/listDoctor', 
-      icon: <FaUserDoctor style={{ color: 'black' }} />,
+      icon: <LocalHospitalIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'Receptionist',
       path: '/admin/listReceptionist',
-      icon: <HowToRegIcon />,
+      icon: <PersonalVideoIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'Lab',
       path: '/admin/listLab',
-      icon: <TbReportSearch style={{ color: 'black' }} />,
+      icon: <ReceiptIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'User Requests',
+      path: '/admin/userRequests',
+      icon: <PersonPinIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Change Password',
+      path: '/admin/changePassword',
+      icon: <LockOpenIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Logout',
       path: '/',
-      icon: <FaRegUser style={{ color: 'black' }} />,
+      icon: <LogoutIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
   ],
   doctor: [
     {
       title: 'Home',
-      path: '/',
-      icon: <HomeIcon />,
+      path: '/doctor',
+      icon: <HomeIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
     {
       title: 'Patients',
-      path: '/doctor', // Corrected path
-      icon: <PersonAddAltIcon style={{ color: 'black' }} />,
+      path: '/doctor/listPatients', 
+      icon: <PersonAddAltIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
     {
       title: 'Doctors',
-      path: '/',
-      icon: <FaUserDoctor style={{ color: 'black' }} />,
+      path: '/doctor/listDoctors',
+      icon: <LocalHospitalIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'Reports',
-      path: '/',
-      icon: <TbReportSearch />,
+      path: '/doctor/',
+      icon: <ReceiptIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'View Invitations',
+      path: '/doctor/viewInvitations',
+      icon: <DraftsIcon className="icon-hover" />,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Change Password',
+      path: '/doctor/changePassword',
+      icon: <LockOpenIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Logout',
       path: '/',
-      icon: <DraftsIcon style={{ color: 'black' }} />,
+      icon: <LogoutIcon className="icon-hover" />,
       cName: 'nav-text'
     },
   ],
   lab: [
     {
       title: 'Home',
-      path: '/',
-      icon: <HomeIcon />,
+      path: '/lab',
+      icon: <HomeIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
     {
       title: 'Reports',
-      path: '/',
-      icon: <TbReportSearch />,
+      path: '/lab/upload',
+      icon: <ReceiptIcon/>,
       cName: 'nav-text'
     },
-
+    {
+      title: 'Change Password',
+      path: '/admin/changePassword',
+      icon: <LockOpenIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Logout',
+      path: '/',
+      icon: <LogoutIcon className="icon-hover" />,
+      cName: 'nav-text'
+    },
   ],
   receptionist: [
     {
       title: 'Home',
-      path: '/',
-      icon: <HomeIcon />,
+      path: '/receptionist',
+      icon: <HomeIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
     {
       title: 'Register Patient',
       path: '/receptionist/addPatient',
-      icon: <BsPeopleFill/>,
+      icon: <PeopleIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Change Password',
+      path: '/receptionist/changePassword',
+      icon: <LockOpenIcon/>,
       cName: 'nav-text'
     },
     {
       title: 'New Case',
-      path: '/',
-      icon: <PiNotebookBold />,
+      path: '/receptionist/newCase',
+      icon: <  ContentCopyIcon/>,
       cName: 'nav-text'
     },
-
-
   ],
   radiologist:[
     {
       title: 'Home',
-      path: '/',
-      icon: <HomeIcon />,
+      path: '/radiologist',
+      icon: <HomeIcon className="icon-hover"/>,
       cName: 'nav-text'
     },
-    
-
+    {
+      title: 'View Invitations',
+      path: '/radiologist/viewInvitations',
+      icon: <DraftsIcon className="icon-hover" />,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Active Cases',
+      path: '/radiologist/viewCases',
+      icon: <ReceiptIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Change Password',
+      path: '/radiologist/changePassword',
+      icon: <LockOpenIcon/>,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Logout',
+      path: '/',
+      icon: <LogoutIcon className="icon-hover" />,
+      cName: 'nav-text'
+    },
   ]
 };
+
+
