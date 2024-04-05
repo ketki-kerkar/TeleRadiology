e413b98b51e3b3c38be37ecda06ba0918cb3c3ae
Ketki Kerkar <ketki.kerkar@iiitb.ac.in>
2024-04-06 01:16:31 +0530

    Updated view pages for integration

--- FRONTEND/src/Actors/Admin/ViewDoctor.jsx
+++ FRONTEND/src/Actors/Admin/ViewDoctor.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React ,{useEffect,useState}from 'react';
 import { Box} from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CustomButton from '../../Components/Button';
 import Navbar from '../../Components/Navbar';
 import Profile from '../../Images/icon1.svg';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function ViewDoctor() {
+  const [userData, setUserData] = useState([]);
+  const [error, setError] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('http://localhost:9191/api/v1/admin/findUser/ByEmail', {
+          email: "ketki@gmail.com" // Provide the email you want to search for
+        });
+    
+        // Check if response data is not null or undefined before setting state
+        if (response.data) {
+          setUserData(response.data);
+          setError(null);
+          console.log(response.data);
+         
+        } else {
+          // Handle case where response data is null or undefined
+          setError('User data not found');
+          setUserData(null);
+        }
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+        // Handle error, show a message to the user maybe
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+
+  const handleDeleteUser = async () => {
+    // Delete user logic here
+    try {
+      const response = await axios.put('http://localhost:9191/api/v1/admin/delete-user', {
+        email: "ketki@gmail.com" // Provide the email of the doctor to be deleted
+      });
+      navigate('/admin');
+      // Handle successful response, maybe update UI or show a success message
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error disabling doctor:', error);
+      // Handle error, show a message to the user maybe
+    }
+   
+  };
+
   return (
     <div>
         <Navbar userRole ="admin"/>
@@ -19,30 +70,35 @@ export default function ViewDoctor() {
                   height: '70vh', width: '37%', borderRadius: '16px',
                   border: '0.2vw solid #9ACEFF',
                   }} >
+                  
                   <div style={{ display: 'flex', flexDirection: 'row',justifyContent: 'center',alignItems: 'center', width: '100%', marginTop:'2vh',marginBottom:'2vh'}}>
                     <Box  bgcolor="#f0f0e7" sx={{height:'19vh',width:'8.8vw',borderRadius:'50%', border: '0.01vw solid black',marginRight:'1vw'}}>
                     </Box>
 
                     <Box alignSelf='center' color='black' bgcolor='#D1EEF2' sx={{marginLeft:'1vw',display: 'flex',justifyContent: 'center', alignItems: 'center', height:'9vh',width:'15vw',borderRadius:'1vw', border: '0.1vw solid #6CC4BF',fontFamily: 'Quicksand, sans-serif',
                        fontSize: '1.3vw',}}>
-                        <img src={Profile} style={{ marginRight: '10px', width: '24px', height: '24px' }} alt="profile"/>
-                               Dr. Travis Scott
+                        <img src={Profile} style={{ marginRight: '10px', width: '24px', height: '24px' }} alt='Profile'/>
+                        {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].dname: 'N/A'}
+                        
                     </Box>
                     </div>                
                 <Box bgcolor="white" sx={{ marginBottom:'7vh',marginTop:'2vh',height: '40vh', width: '90%', borderRadius: '1vw',fontFamily: 'Quicksand, sans-serif',
                       fontSize: '1.3vw'}}>
                 <div style={{ textAlign: 'left',marginLeft:'3vw',marginBottom:'3vh',marginTop:'3vh' }}>
-            <p> <ChevronRightIcon /> User Id: Travis scot</p>
-            <p><ChevronRightIcon />  Doctor Type: Radiologist</p>
-            <p><ChevronRightIcon /> Qualification: abc</p>
-            <p><ChevronRightIcon /> Hospital Name: abc</p>
-            <p><ChevronRightIcon /> Department: Radiology</p>
-            <p><ChevronRightIcon /> Email Id: abc@gmail.com</p>
+                <p> <ChevronRightIcon /> Doctor Name: {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].dname: 'N/A'}</p>
+            <p><ChevronRightIcon />  Doctor Type: {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].dtype : 'N/A'}</p>
+            <p><ChevronRightIcon /> Qualification: {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].qualification : 'N/A'}</p>
+            <p><ChevronRightIcon /> Hospital Name: {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].hospitalName : 'N/A'}</p>
+            <p><ChevronRightIcon /> Department: {userData && userData.doctors && userData.doctors.length > 0 ? userData.doctors[0].department : 'N/A'}</p>
+            <p><ChevronRightIcon /> Email Id: {userData && userData.email}</p>
+           
             
           </div>
                 </Box>
             </Box>
-            <CustomButton></CustomButton>
+
+            
+            <CustomButton onClick={handleDeleteUser}></CustomButton>
     </div>
     </div>
   )
--- FRONTEND/src/Actors/Admin/ViewHospital.jsx
+++ FRONTEND/src/Actors/Admin/ViewHospital.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React,{useEffect,useState}  from 'react';
 import { Box } from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CustomButton from '../../Components/Button';
 import Navbar from '../../Components/Navbar';
 import Profile from '../../Images/icon1.svg';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function ViewHospital() {
+  const [hospitalData, setHospitalData] = useState([]);
+  const [error, setError] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    fetchHospitalData();
+  }, []);
+
+  const fetchHospitalData = async () => {
+    try {
+      const response = await axios.post('http://localhost:9191/api/v1/admin/findUser/ByEmail', {
+        email: 'godeanuja5@gmail.com' // Provide the email you want to search for
+      });
+      setHospitalData(response.data);
+      console.log(response.data);
+      setError(null);
+    } catch (error) {
+      console.error('Error fetching hospital data:', error);
+      setError('Failed to fetch hospital data');
+      setHospitalData(null);
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    // Delete user logic here
+    try {
+      const response = await axios.put('http://localhost:9191/api/v1/admin/delete-user', {
+        email: "godeanuja5@gmail.com" // Provide the email of the doctor to be deleted
+      });
+      navigate('/admin');
+      // Handle successful response, maybe update UI or show a success message
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error disabling doctor:', error);
+      // Handle error, show a message to the user maybe
+    }
+   
+  };
+
+
+
+
+
   return (
     <div>
         <Navbar userRole ="admin"/>
@@ -25,20 +69,20 @@ export default function ViewHospital() {
                     <Box alignSelf='center' color='black' bgcolor='#D1EEF2' sx={{marginLeft:'1vw',display: 'flex',justifyContent: 'center', alignItems: 'center', height:'9vh',width:'15vw',borderRadius:'1vw', border: '0.1vw solid #6CC4BF',fontFamily: 'Quicksand, sans-serif',
                        fontSize: '1.3vw',}}>
                         <img src={Profile} style={{ marginRight: '10px', width: '24px', height: '24px' }} alt='Profile'/>
-                               Dr. Travis Scott
+                        {hospitalData && hospitalData.hospitalHandles && hospitalData.hospitalHandles.length > 0 ? hospitalData.hospitalHandles[0].hospitalName : 'N/A'}
                     </Box>
                     </div>                
                 <Box bgcolor="white" sx={{ marginBottom:'7vh',marginTop:'2vh',height: '40vh', width: '90%', borderRadius: '1vw',fontFamily: 'Quicksand, sans-serif',
                       fontSize: '1.3vw'}}>
                 <div style={{ textAlign: 'left',marginLeft:'3vw',marginBottom:'3vh',marginTop:'3vh' }}>
             <p> <ChevronRightIcon /> Id: 454748bfjeejd9</p>
-            <p><ChevronRightIcon /> Hospital name: abc</p>
-            <p><ChevronRightIcon /> Email Id: abc@gmail.com</p>
+            <p><ChevronRightIcon /> Hospital name:  {hospitalData && hospitalData.hospitalHandles && hospitalData.hospitalHandles.length > 0 ? hospitalData.hospitalHandles[0].hospitalName : 'N/A'}</p>
+            <p><ChevronRightIcon /> Email Id: {hospitalData && hospitalData.email}</p>
             
           </div>
                 </Box>
             </Box>
-            <CustomButton/>
+            <CustomButton onClick={handleDeleteUser}/>
     </div>
     </div>
     
