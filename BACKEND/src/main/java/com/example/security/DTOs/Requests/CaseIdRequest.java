ea2087253b72dcf4b13186287e4e2e003dc2d678
AnkitaAgrawal12 <AnkitaAgrawal@iiitb.ac.in>
2024-05-01 11:53:58 +0530

    committing patient backend

---
+++ BACKEND/src/main/java/com/example/security/DTOs/Requests/CaseIdRequest.java
@@ -0,0 +1,13 @@
+package com.example.security.DTOs.Requests;
+
+public class CaseIdRequest {
+    private Long caseId;
+
+    public Long getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Long caseId) {
+        this.caseId = caseId;
+    }
+}
