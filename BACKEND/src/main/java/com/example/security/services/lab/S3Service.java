92faec3f7df4a402ac67d50d00d42b10e2051e1e
Ketki Kerkar <ketki.kerkar@iiitb.ac.in>
2024-05-01 14:54:57 +0530

    aws upload function added for Lab reports

---
+++ BACKEND/src/main/java/com/example/security/services/lab/S3Service.java
@@ -0,0 +1,31 @@
+package com.example.security.services.lab;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@Log4j
+public class S3Service {
+    private AmazonS3 s3client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(AmazonS3 s3client) {
+        this.s3client = s3client;
+    }
+
+    public void uploadFile(String keyName, MultipartFile file) throws IOException {
+        var putObjectResult = s3client.putObject(bucketName, keyName, file.getInputStream(), null);
+    }
+
+    public S3Object getFile(String keyName) {
+        return s3client.getObject(bucketName, keyName);
+    }
+}
