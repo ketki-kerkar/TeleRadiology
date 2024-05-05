fcb5208b2d1ba9daf16e31fb0ae57c14218bf22a
AnkitaAgrawal12 <AnkitaAgrawal@iiitb.ac.in>
2024-05-05 15:18:20 +0530

    backend code of view patient details

--- BACKEND/src/main/java/com/example/security/Repositories/PatientRepo.java
+++ BACKEND/src/main/java/com/example/security/Repositories/PatientRepo.java
@@ -1,5 +1,5 @@
 package com.example.security.Repositories;
-import com.example.security.DTOs.PatientDTO;
+
 import com.example.security.Model.Actors.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,11 +13,8 @@ import java.util.UUID;
 public interface PatientRepo extends JpaRepository<Patient, Long> {
     List<Patient> findAll();
     Optional<Patient> findByUserEmail(String emailId);
-
     Optional<Patient> findByUser_Email(String email);
-
     Optional<Patient> findByUserUserId(UUID userId);
-
     @Query("SELECT patient FROM Patient patient WHERE patient.user.userId = :userId")
     Optional<Patient> findByUUId(UUID userId);
 }
